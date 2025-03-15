25ad21d6a6733365c6366d776ec41572b5e6b55e
Prince <2023ugec056@nitjsr.ac.in>
2025-03-16 01:44:04 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./.env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("mongo db conection failed!!!!",error)
+})
 
 
 
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,23 @@
+const asynchandler=(requesthandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next))
+        .catch((err)=>next(err))
+
+    }
+}
+
+
+
+//* const abc=(fn)=>{()=>{}}  dono same hai
+// const asynchandler=(fn)=>async(req,res,next)=>{
+    
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+        
+//     }
+// }
