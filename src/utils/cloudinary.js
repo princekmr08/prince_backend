9f15a28d101886af2603534a4385bc9c08034e7b
Prince <2023ugec056@nitjsr.ac.in>
2025-03-16 23:43:36 +0530

    upload file in cloudinary using multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary=async (localfilePath)=>{
+    try {
+        if(!localfilePath) return null;
+        // upload file on cloudinary
+        const response=await cloudinary.uploader.upload(
+            localfilePath,{
+                resource_type:"auto"
+            }
+        )
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudianry ",response.url)
+        return response;
+    } catch (error) {
+        fs.unlink(localfilePath)// remove the locally 
+        // saved temporary file as upload file is failed
+        
+        
+    }
+}
